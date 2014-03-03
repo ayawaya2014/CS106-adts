8402f6c3018e26cbb95942201a43531918e7bcb2
ayawaya2014 <lixin850224@163.com>
2014-03-03 21:03:59 +0800

    Create maze-generator.cpp

---
+++ maze-generator.cpp
@@ -0,0 +1,26 @@
+/**
+ * File: maze-generator.cpp
+ * ------------------
+ * Name:
+ * Section Leader Name:
+ */
+#include "foreach.h"
+#include <iostream>
+#include "set.h"
+#include "vector.h"
+#include "map.h"
+#include "genlib.h"
+#include "extgraph.h"
+#include "simpio.h"
+#include "random.h"
+#include "maze-types.h"
+#include "maze-graphics.h"
+
+
+
+//Make me work!
+int main() 
+{
+	InitMazeGraphics();
+	return 0;
+}
